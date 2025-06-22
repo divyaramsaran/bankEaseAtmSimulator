73b7d98d67b17388bec1c4e672bf37a2e4c74c37
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-06-22 07:39:04 +0000

    added user password

--- ATM.js
+++ ATM.js
@@ -1,15 +1,26 @@
 class ATM {
   #userName;
+  #pin;
   set userName(name) {
     this.#userName = name;
   }
   get userName() {
     return this.#userName;
   }
+  set pin(password) {
+    this.#pin = password;
+  }
+
+  get pin() {
+    return this.#pin;
+  }
+
   display(obj) {
-    return obj.#userName;
+    console.log(obj.#userName);
+    console.log(obj.#pin);
   }
 }
 const obj = new ATM();
-obj.userName = prompt("enter userName");
-console.log(obj.display(obj));
+obj.userName = prompt("enter userName", "ramsaran");
+obj.pin = Number(prompt("enter you ATM pin", 1456788));
+obj.display(obj);
