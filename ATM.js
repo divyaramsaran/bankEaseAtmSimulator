3ac84b9ddc102188afaa0509fb67b5e5c1a14231
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-06-22 11:34:38 +0000

    added operations input method and validation

--- ATM.js
+++ ATM.js
@@ -1,26 +1,53 @@
 class ATM {
   #userName;
   #pin;
+
   set userName(name) {
     this.#userName = name;
   }
   get userName() {
     return this.#userName;
   }
+
   set pin(password) {
     this.#pin = password;
   }
-
   get pin() {
     return this.#pin;
   }
 
-  display(obj) {
-    console.log(obj.#userName);
-    console.log(obj.#pin);
+  operations() {
+    console.log(`1.Withdraw
+2.Deposit
+3.Balance Inquiry
+4.Change PIN
+5.Exit`);
+    return Number(prompt("Enter choice:"));
+  }
+
+  validateOperation(operation, obj, incorrectAttempts) {
+    if ((operation > 5 || operation < 1) && incorrectAttempts !== 0) {
+      incorrectAttempts--;
+
+      console.clear();
+      console.log("Incorrect Input Attemps Left", incorrectAttempts);
+      return this.computeOperations(obj, incorrectAttempts);
+    }
+    if (incorrectAttempts === 0) {
+      console.log(
+        "Card locked due to too many failed attempts. Please contact your bank."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  computeOperations(obj, incorrectAttempts) {
+    const operation = this.operations();
+    return this.validateOperation(operation, obj, incorrectAttempts);
   }
 }
 const obj = new ATM();
 obj.userName = prompt("enter userName", "ramsaran");
 obj.pin = Number(prompt("enter you ATM pin", 1456788));
-obj.display(obj);
+obj.computeOperations(obj, 3);
